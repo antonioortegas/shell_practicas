7cf7d1fde2f04479a38fbe16fcf3d39d2c5201a9
antonioortegas <antonioortega@uma.es>
2024-05-26 00:27:13 +0200

    tarea 4

--- Shell_project.c
+++ Shell_project.c
@@ -19,6 +19,43 @@ To compile and run the program:
 
 #define MAX_LINE 256 /* 256 chars per line, per command, should be enough. */
 
+job *list;
+
+void handler(int signal){
+    //signal is 17 always, since we only handle 1 signal
+    //this is executed when any of the child processes exit or are suspended
+    job *item = NULL;
+    int status;
+    int info;
+    int index = 1; // LIST STARTS ON 1, I GUESS WE ARE DOING LUA INDEXING NOW
+    int pid_wait;
+    int status_res;
+
+    while(index <= list_size(list)){
+        item = get_item_bypos(list, index);
+        index++;
+        //call waitpid with 2 options ( | bitwise operator )
+        //WNOHANG so it does not hang waiting for unfinished processes, since some of the jobs are still running
+        pid_wait = waitpid(item->pgid, &status, WUNTRACED | WNOHANG);
+        if(pid_wait == item->pgid){
+            //if waitpid returns, that job has been updated (suspended or exited)
+            status_res = analyze_status(status, &info);
+            if(status_res == SUSPENDED){
+                printf("El proceso en segundo plano %s con pid %d se ha suspendido", item->command, item->pgid);
+                fflush;
+                //if the process was suspended, update the list with the new information
+                item->state = SUSPENDED;
+            } else if(status_res == EXITED){
+                printf("El proceso en segundo plano %s con pid %d ha terminado su ejecucion", item->command, item->pgid);
+                fflush;
+                //since the process has finished, we can delete it from our list
+                delete_job(list, item);
+            }
+            break;
+        }
+    }
+}
+
 // -----------------------------------------------------------------------
 //                            MAIN
 // -----------------------------------------------------------------------
@@ -35,6 +72,12 @@ int main(void) {
     int info;               /* info processed by analyze_status() */
 
     ignore_terminal_signals();
+
+    //install sigchld handler
+    signal(SIGCHLD, handler);
+
+    job *item;
+    list = new_list("list");
     
     while (1) /* Program terminates normally inside get_command() after ^D is typed*/
     {
@@ -82,9 +125,11 @@ int main(void) {
         } else {
             // parent
             if (background) {
+                // make a new job and add it to the list
+                item = new_job(pid_fork, args[0], BACKGROUND);
+                add_job(list, item);
                 printf("Background job running... pid: %d, command: %s\n", pid_fork, args[0]);
             } else {
-                
                 //wait for child to finish
                 pid_wait = waitpid(pid_fork, &status, WUNTRACED);  // wait for child process
                 //get the terminal back
@@ -93,6 +138,12 @@ int main(void) {
                 status_res = analyze_status(status, &info);
                 if (info != 1) {
                     printf("Foreground pid: %d, command: %s %s info: %d\n", pid_fork, args[0], status_strings[status_res], info);
+                }
+                //if it was suspended, add it to the list
+                if(status_res == SUSPENDED){
+                    item = new_job(pid_fork, args[0], BACKGROUND);
+                    add_job(list, item);
+                    printf("Añadido el proceso %s, con pid %d a la lista de jobs suspendidos\n", args[0], pid_fork);
                 }
             }
         }
